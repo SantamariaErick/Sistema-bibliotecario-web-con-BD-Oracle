fdd5cc77c794142582c219498524e2dc85e84ffd
ErickSantamaria <ejsantamaria1@espe.edu.ec>
2021-09-11 19:39:40 -0500

    Base de datos actualizacion
    Pilas las instrucciones

--- Diagramas/Biblioteca.sql
+++ Diagramas/Biblioteca.sql
@@ -1,165 +1,248 @@
 /*==============================================================*/
-/* DBMS name:      MySQL 5.0                                    */
-/* Created on:     9/9/2021 21:37:21                            */
+/* DBMS name:      ORACLE Version 10g                           */
+/* Created on:     11/9/2021 19:25:21                           */
 /*==============================================================*/
 
 
-drop table if exists AUDITORIA;
+alter table AUDITORIA
+   drop constraint FK_AUDITORI_RELATIONS_PRESTAMO;
 
-drop table if exists AUTOR;
+alter table AUDITORIA
+   drop constraint FK_AUDITORI_RELATIONS_USUARIO;
 
-drop table if exists EDITORIAL;
+alter table LIBRO
+   drop constraint FK_LIBRO_DIFERENTE_MATERIA;
 
-drop table if exists ESTUDIANTE;
+alter table LIBRO
+   drop constraint FK_LIBRO_ESCRIBE_AUTOR;
 
-drop table if exists LIBRO;
+alter table LIBRO
+   drop constraint FK_LIBRO_PERTENECE_EDITORIA;
 
-drop table if exists MATERIA;
+alter table PRESTAMO
+   drop constraint FK_PRESTAMO_DE_LIBRO;
 
-drop table if exists PRESTAMO;
+alter table PRESTAMO
+   drop constraint FK_PRESTAMO_REALIZA_ESTUDIAN;
 
-drop table if exists USUARIO;
+drop index RELATIONSHIP_7_FK;
+
+drop index RELATIONSHIP_6_FK;
+
+drop table AUDITORIA cascade constraints;
+
+drop table AUTOR cascade constraints;
+
+drop table EDITORIAL cascade constraints;
+
+drop table ESTUDIANTE cascade constraints;
+
+drop index ESCRIBE_FK;
+
+drop index PERTENECE_FK;
+
+drop index DIFERENTES_FK;
+
+drop table LIBRO cascade constraints;
+
+drop table MATERIA cascade constraints;
+
+drop index DE_FK;
+
+drop index REALIZA_FK;
+
+drop table PRESTAMO cascade constraints;
+
+drop table USUARIO cascade constraints;
 
 /*==============================================================*/
 /* Table: AUDITORIA                                             */
 /*==============================================================*/
-create table AUDITORIA
-(
-   AUD_ID               int not null auto_increment,
-   PRE_ID               int,
-   USU_ID               int not null,
-   AUD_DESCRIPCION      varchar(50) not null,
-   primary key (AUD_ID)
+create table AUDITORIA  (
+   AUD_ID               INTEGER                         not null,
+   PRE_ID               INTEGER,
+   USU_ID               INTEGER                         not null,
+   AUD_DESCRIPCION      VARCHAR2(50)                    not null,
+   constraint PK_AUDITORIA primary key (AUD_ID)
+);
+
+/*==============================================================*/
+/* Index: RELATIONSHIP_6_FK                                     */
+/*==============================================================*/
+create index RELATIONSHIP_6_FK on AUDITORIA (
+   PRE_ID ASC
+);
+
+/*==============================================================*/
+/* Index: RELATIONSHIP_7_FK                                     */
+/*==============================================================*/
+create index RELATIONSHIP_7_FK on AUDITORIA (
+   USU_ID ASC
 );
 
 /*==============================================================*/
 /* Table: AUTOR                                                 */
 /*==============================================================*/
-create table AUTOR
-(
-   AUT_ID               int not null auto_increment,
-   AUT_NOMBRE           varchar(50) not null,
-   AUT_DESCRIPCION      varchar(300) not null,
-   AUT_IMAGEN           varchar(50) not null,
-   AUT_ESTADO           bool not null,
-   primary key (AUT_ID)
+create table AUTOR  (
+   AUT_ID               INTEGER                         not null,
+   AUT_NOMBRE           VARCHAR2(50)                    not null,
+   AUT_DESCRIPCION      VARCHAR2(300)                   not null,
+   AUT_IMAGEN           VARCHAR2(50)                    not null,
+   AUT_ESTADO           SMALLINT                        not null,
+   constraint PK_AUTOR primary key (AUT_ID)
 );
 
 /*==============================================================*/
 /* Table: EDITORIAL                                             */
 /*==============================================================*/
-create table EDITORIAL
-(
-   EDI_ID               int not null auto_increment,
-   EDI_NOMBRE           varchar(50) not null,
-   EDI_DESCRIPCION      varchar(300) not null,
-   EDI_CONDICION        bool not null,
-   primary key (EDI_ID)
+create table EDITORIAL  (
+   EDI_ID               INTEGER                         not null,
+   EDI_NOMBRE           VARCHAR2(50)                    not null,
+   EDI_DESCRIPCION      VARCHAR2(300)                   not null,
+   EDI_CONDICION        SMALLINT                        not null,
+   constraint PK_EDITORIAL primary key (EDI_ID)
 );
 
 /*==============================================================*/
 /* Table: ESTUDIANTE                                            */
 /*==============================================================*/
-create table ESTUDIANTE
-(
-   EST_ID               int not null auto_increment,
-   EST_CODIGO           varchar(20) not null,
-   EST_CEDULA           varchar(10) not null,
-   EST_NOMBRE           varchar(50) not null,
-   EST_CARRERA          varchar(30) not null,
-   EST_DIRECCION        varchar(50),
-   EST_TELEFONO         varchar(10) not null,
-   EST_EMAIL            varchar(50) not null,
-   EST_ESTADO           bool not null,
-   primary key (EST_ID)
+create table ESTUDIANTE  (
+   EST_ID               INTEGER                         not null,
+   EST_CODIGO           VARCHAR2(20)                    not null,
+   EST_CEDULA           VARCHAR2(10)                    not null,
+   EST_NOMBRE           VARCHAR2(50)                    not null,
+   EST_CARRERA          VARCHAR2(30)                    not null,
+   EST_DIRECCION        VARCHAR2(50),
+   EST_TELEFONO         VARCHAR2(10)                    not null,
+   EST_EMAIL            VARCHAR2(50)                    not null,
+   EST_ESTADO           SMALLINT                        not null,
+   constraint PK_ESTUDIANTE primary key (EST_ID)
 );
 
 /*==============================================================*/
 /* Table: LIBRO                                                 */
 /*==============================================================*/
-create table LIBRO
-(
-   LIB_ID               int not null auto_increment,
-   MAT_ID               int not null,
-   EDI_ID               int not null,
-   AUT_ID               int not null,
-   LIB_TITULO           varchar(50) not null,
-   LIB_CANTIDADDISPONIBLE numeric(10,0) not null,
-   LIB_ANIOEDICION      varchar(4) not null,
-   LIB_CANTIDADPAGINAS  numeric(10,0) not null,
-   LIB_FORMATO          varchar(50) not null,
-   LIB_PESO             decimal(7,2) not null,
-   LIB_DESCIPCION       varchar(500) not null,
-   LIB_PORTADA          varchar(50) not null,
-   LIB_ESTADO           bool not null,
-   primary key (LIB_ID)
+create table LIBRO  (
+   LIB_ID               INTEGER                         not null,
+   MAT_ID               INTEGER                         not null,
+   EDI_ID               INTEGER                         not null,
+   AUT_ID               INTEGER                         not null,
+   LIB_TITULO           VARCHAR2(50)                    not null,
+   LIB_CANTIDADDISPONIBLE NUMBER(10)                      not null,
+   LIB_ANIOEDICION      VARCHAR2(4)                     not null,
+   LIB_CANTIDADPAGINAS  NUMBER(10)                      not null,
+   LIB_FORMATO          VARCHAR2(50)                    not null,
+   LIB_PESO             NUMBER(7,2)                     not null,
+   LIB_DESCIPCION       VARCHAR2(500)                   not null,
+   LIB_PORTADA          VARCHAR2(50)                    not null,
+   LIB_ESTADO           SMALLINT                        not null,
+   constraint PK_LIBRO primary key (LIB_ID)
+);
+
+/*==============================================================*/
+/* Index: DIFERENTES_FK                                         */
+/*==============================================================*/
+create index DIFERENTES_FK on LIBRO (
+   MAT_ID ASC
+);
+
+/*==============================================================*/
+/* Index: PERTENECE_FK                                          */
+/*==============================================================*/
+create index PERTENECE_FK on LIBRO (
+   EDI_ID ASC
+);
+
+/*==============================================================*/
+/* Index: ESCRIBE_FK                                            */
+/*==============================================================*/
+create index ESCRIBE_FK on LIBRO (
+   AUT_ID ASC
 );
 
 /*==============================================================*/
 /* Table: MATERIA                                               */
 /*==============================================================*/
-create table MATERIA
-(
-   MAT_ID               int not null auto_increment,
-   MAT_NOMBRE           varchar(50) not null,
-   MAT_DESCRIPCION      varchar(300) not null,
-   MAT_CONDICION        bool not null,
-   primary key (MAT_ID)
+create table MATERIA  (
+   MAT_ID               INTEGER                         not null,
+   MAT_NOMBRE           VARCHAR2(50)                    not null,
+   MAT_DESCRIPCION      VARCHAR2(300)                   not null,
+   MAT_CONDICION        SMALLINT                        not null,
+   constraint PK_MATERIA primary key (MAT_ID)
 );
 
 /*==============================================================*/
 /* Table: PRESTAMO                                              */
 /*==============================================================*/
-create table PRESTAMO
-(
-   PRE_ID               int not null auto_increment,
-   EST_ID               int not null,
-   LIB_ID               int not null,
-   PRE_FECHAPRESTADO    date not null,
-   PRE_FECHADEVUELTO    date not null,
-   PRE_CANTIDAD         numeric(10,0) not null,
-   PRE_OBSERVACIONES    varchar(150) not null,
-   PRE_ESTADO           varchar(8) not null,
-   primary key (PRE_ID)
+create table PRESTAMO  (
+   PRE_ID               INTEGER                         not null,
+   EST_ID               INTEGER                         not null,
+   LIB_ID               INTEGER                         not null,
+   PRE_FECHAPRESTADO    DATE                            not null,
+   PRE_FECHADEVUELTO    DATE                            not null,
+   PRE_CANTIDAD         NUMBER(10)                      not null,
+   PRE_OBSERVACIONES    VARCHAR2(150)                   not null,
+   PRE_ESTADO           VARCHAR2(8)                     not null,
+   constraint PK_PRESTAMO primary key (PRE_ID)
+);
+
+/*==============================================================*/
+/* Index: REALIZA_FK                                            */
+/*==============================================================*/
+create index REALIZA_FK on PRESTAMO (
+   EST_ID ASC
+);
+
+/*==============================================================*/
+/* Index: DE_FK                                                 */
+/*==============================================================*/
+create index DE_FK on PRESTAMO (
+   LIB_ID ASC
 );
 
 /*==============================================================*/
 /* Table: USUARIO                                               */
 /*==============================================================*/
-create table USUARIO
-(
-   USU_ID               int not null auto_increment,
-   USU_TRABAJADOR       varchar(30) not null,
-   USU_CEDULA           varchar(11) not null,
-   USU_NOMBRE           varchar(50) not null,
-   USU_PROFESION        varchar(30) not null,
-   USU_CARGO            varchar(20) not null,
-   USU_DIRECCION        varchar(50) not null,
-   USU_TELEFONO         varchar(10) not null,
-   USU_EMAIL            varchar(30) not null,
-   USU_LOGIN            varchar(30) not null,
-   USU_CLAVE            varchar(30) not null,
-   primary key (USU_ID)
+create table USUARIO  (
+   USU_ID               INTEGER                         not null,
+   USU_TRABAJADOR       VARCHAR2(30)                    not null,
+   USU_CEDULA           VARCHAR2(11)                    not null,
+   USU_NOMBRE           VARCHAR2(50)                    not null,
+   USU_PROFESION        VARCHAR2(30)                    not null,
+   USU_CARGO            VARCHAR2(20)                    not null,
+   USU_DIRECCION        VARCHAR2(50)                    not null,
+   USU_TELEFONO         VARCHAR2(10)                    not null,
+   USU_EMAIL            VARCHAR2(30)                    not null,
+   USU_LOGIN            VARCHAR2(30)                    not null,
+   USU_CLAVE            VARCHAR2(30)                    not null,
+   constraint PK_USUARIO primary key (USU_ID)
 );
 
-alter table AUDITORIA add constraint FK_RELATIONSHIP_6 foreign key (PRE_ID)
-      references PRESTAMO (PRE_ID) on delete restrict on update restrict;
+alter table AUDITORIA
+   add constraint FK_AUDITORI_RELATIONS_PRESTAMO foreign key (PRE_ID)
+      references PRESTAMO (PRE_ID);
 
-alter table AUDITORIA add constraint FK_RELATIONSHIP_7 foreign key (USU_ID)
-      references USUARIO (USU_ID) on delete restrict on update restrict;
+alter table AUDITORIA
+   add constraint FK_AUDITORI_RELATIONS_USUARIO foreign key (USU_ID)
+      references USUARIO (USU_ID);
 
-alter table LIBRO add constraint FK_DIFERENTES foreign key (MAT_ID)
-      references MATERIA (MAT_ID) on delete restrict on update restrict;
+alter table LIBRO
+   add constraint FK_LIBRO_DIFERENTE_MATERIA foreign key (MAT_ID)
+      references MATERIA (MAT_ID);
 
-alter table LIBRO add constraint FK_ESCRIBE foreign key (AUT_ID)
-      references AUTOR (AUT_ID) on delete restrict on update restrict;
+alter table LIBRO
+   add constraint FK_LIBRO_ESCRIBE_AUTOR foreign key (AUT_ID)
+      references AUTOR (AUT_ID);
 
-alter table LIBRO add constraint FK_PERTENECE foreign key (EDI_ID)
-      references EDITORIAL (EDI_ID) on delete restrict on update restrict;
+alter table LIBRO
+   add constraint FK_LIBRO_PERTENECE_EDITORIA foreign key (EDI_ID)
+      references EDITORIAL (EDI_ID);
 
-alter table PRESTAMO add constraint FK_DE foreign key (LIB_ID)
-      references LIBRO (LIB_ID) on delete restrict on update restrict;
+alter table PRESTAMO
+   add constraint FK_PRESTAMO_DE_LIBRO foreign key (LIB_ID)
+      references LIBRO (LIB_ID);
 
-alter table PRESTAMO add constraint FK_REALIZA foreign key (EST_ID)
-      references ESTUDIANTE (EST_ID) on delete restrict on update restrict;
+alter table PRESTAMO
+   add constraint FK_PRESTAMO_REALIZA_ESTUDIAN foreign key (EST_ID)
+      references ESTUDIANTE (EST_ID);
 
