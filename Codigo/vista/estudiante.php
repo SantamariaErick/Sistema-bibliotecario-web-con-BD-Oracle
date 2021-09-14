a73d0178c162cede2100409298dc75fac47f7cc7
KvnSG <kevinsuntaxi@hotmail.com>
2021-09-14 01:58:20 -0500

    Mostrar y agregar estudiante

---
+++ Codigo/vista/estudiante.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Documento sin título</title>
+</head>
+
+<body>
+<!--Contenido-->
+  <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+	<?php
+		$conexion = oci_connect('BIBLIOTECA', 'bibliotecaweb', 'localhost/orcl');
+		//Ejecutamos la sentencia SQL
+		$stid = oci_parse($conexion, "select * from estudiante");
+		oci_execute($stid);
+	?>        
+	<!-- Main content -->
+	<section class="content">
+		<div class="row">
+		  <div class="col-md-12">
+			  <div class="box">
+				<div class="box-header with-border">
+					  <h1 class="box-title">Estudiante 	  
+					  <a href="formulario_estudiante.php" id="idcrear" ><button class="btn btn-success" id="btnagregar" onclick="mostrarform(true)"><i class="fa fa-plus-circle"></i> Agregar</button></a></h1>
+					  
+					<div class="box-tools pull-right">
+					</div>
+				</div>
+				<!-- /.box-header -->
+				<!-- centro -->
+				<div class="panel-body table-responsive" id="listadoregistros">
+					<table id="tbllistado" class="table table-striped table-bordered table-condensed table-hover">
+					  <thead>
+						<th>Opciones</th>
+						<th>Codigo</th>
+						<th>DNI</th>
+						<th>Nombre</th>
+						<th>Carrera Profesional</th>
+						<th>Direccion</th>
+						<th>Telefono</th>
+						<th>Email</th>
+						<th>Condición</th>
+					  </thead>
+					  <tbody>
+					  	<?php
+							while ($fila = oci_fetch_array($stid, OCI_ASSOC+OCI_RETURN_NULLS)) {
+								echo "<tr>\n";
+								foreach ($fila as $elemento) {
+									echo "    <td>" . ($elemento !== null ? htmlentities($elemento, ENT_QUOTES) : "") . "</td>\n";
+								}
+								echo "</tr>\n";
+							} 
+						?>                           
+					  </tbody>
+					</table>
+				</div>
+
+				<!--Fin centro -->
+			  </div><!-- /.box -->
+		  </div><!-- /.col -->
+	  </div><!-- /.row -->
+  </section><!-- /.content -->
+
+</div><!-- /.content-wrapper -->
+<!--Fin-Contenido-->
+
+
+</body>
+</html>
+
+
---
+++ Codigo/vista/formulario_estudiante.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Formulario</title>
+<link href="css/estiloform.css" rel="stylesheet" type="text/css">
+<link href="css/estilohtmls.css" rel="stylesheet" type="text/css">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+</head>
+
+<body>
+<div class="cabeza">Ingresar Materio</div>
+<div class="contenido">
+<div class="form-contenedor">
+<form id="form1" name="form1" method="post" action="registro_estudiante.php">
+	<h1 class="centrar titulo">Formulario</h1>
+	
+	<div class="row">
+	<div class="form-dividido2 formulario__grupo" id="grupo__id"><p><label for="id" class="formulario__label">ID </label></p>
+	<input type="number" class="formulario__input" name="id" id="id" placeholder="Ingrese el id" required>
+	</div>
+
+	<div class="form-dividido2 formulario__grupo" id="grupo__codigo"><p><label for="codigo" class="formulario__label">Codigo </label></p>
+	<input type="text" class="formulario__input" name="codigo" id="codigo" placeholder="Ingrese el codigo" required>
+	</div>
+	
+	<div class="form-dividido2 formulario__grupo" id="grupo__cedula"><p><label for="cedula" class="formulario__label">Cedula </label></p>
+	<input type="text" class="formulario__input" name="cedula" id="cedula" placeholder="Ingrese la cedula" required>
+	</div>
+	
+	<div class="form-dividido2 formulario__grupo" id="grupo__nombre"><p><label for="nombre" class="formulario__label">Nombre </label></p>
+	<input type="text" class="formulario__input" name="nombre" id="nombre" placeholder="Ingrese la nombre" required>
+	</div>
+	
+	<div class="form-dividido2 formulario__grupo" id="grupo__carrera"><p><label for="carrera" class="formulario__label">Carrera </label></p>
+	<input type="text" class="formulario__input" name="carrera" id="carrera" placeholder="Ingrese la carrera" required>
+	</div>
+	
+	<div class="form-dividido2 formulario__grupo" id="grupo__direccion"><p><label for="direccion" class="formulario__label">Direccion </label></p>
+	<input type="text" class="formulario__input" name="direccion" id="direccion" placeholder="Ingrese la direccion" required>
+	</div>
+	
+	<div class="form-dividido2 formulario__grupo" id="grupo__telefono"><p><label for="telefono" class="formulario__label">Telefono </label></p>
+	<input type="text" class="formulario__input" name="telefono" id="telefono" placeholder="Ingrese la telefono" required>
+	</div>
+	
+	<div class="form-dividido2 formulario__grupo" id="grupo__email"><p><label for="email" class="formulario__label">Email </label></p>
+	<input type="text" class="formulario__input" name="email" id="email" placeholder="Ingrese la email" required>
+	</div>
+	
+	</div>
+
+
+	<div class="row"><p class="centrar4">
+		<button type="submit" name="enviar" id="idsubmit" value="ingresar"><i class="fas fa-save fa-fw"></i>&nbsp;&nbsp;Guardar</button>
+		<button type="reset" name="resetear" id="idreset" value="limpiar"><i class="fas fa-times-circle fa-fw"></i></button>
+		<div class="volver2"><a href="materia.php" target="_self"><button type="button" name="volver" id="idvolver" value="Volver"><i class="fas fa-chevron-left fa-fw"></i>Volver</button></a></div>
+	</div>
+</form>
+</div>
+</div>
+<script src="js/validaciones.js"></script>
+</body>
+</html>
