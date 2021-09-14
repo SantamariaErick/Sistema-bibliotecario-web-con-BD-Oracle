a58ed4fc8ae0e24377a39f48a78e6433a8eb4128
ErickSantamaria <ejsantamaria1@espe.edu.ec>
2021-09-14 02:09:30 -0500

    Update libro.php

--- Codigo/vista/libro.php
+++ Codigo/vista/libro.php
@@ -3,14 +3,15 @@
 <head>
 <meta charset="utf-8">
 <title>Documento sin título</title>
+	<link href="../public/bootstrap/css/bootstrap.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 	<h3>Libro</h3>
-	<div>Agregar</div>
+	<div><a><input type="button" value="Agregar"></a></div>
 	<?php
 	require('../controlador/Conexion.php');
-	$stid = oci_parse($conexion, "SELECT * FROM libro");
+	$stid = oci_parse($conexion, "SELECT * FROM libro l, autor a, editorial e, materia m where l.aut_id = a.aut_id and l.edi_id = e.edi_id and l.mat_id = m.mat_id");
 	if (!$stid) {
 		$e = oci_error($conexion);
 		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
@@ -23,14 +24,40 @@
 		trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
 	}
 	//comprobar datos de la consulta
-	$row = oci_fetch_array($stid,OCI_ASSOC); 
-	if($row >0){
-		header('location: ../vista/header.php');
-		//Aqui va un header('paginaPrincipal');
-	}
-	else{
-		echo '<script>window.alert("Clave o usuario incorrectos. Ingrese nuevamente"); window.location="../index.html";</script>';
-}
+	 
 	?>
+	<table  class="table table-striped">
+		<thead>
+		<tr>
+			<th>Titulo</th>
+			<th>Disponible</th>
+			<th>Autor</th>
+			<th>Editorial</th>
+			<th>Año Edicion</th>
+			<th>Materia</th>
+			<th>Paginas</th>
+			<th>Formato</th>
+			<th>Peso</th>
+			<th>Descripcion</th>
+			<th>Imagen</th>
+		</tr>
+		</thead>
+	<?php
+		while ( $row = oci_fetch_array($stid) ) {
+			echo '<tr><td>' . $row[ "LIB_TITULO" ] . '</td>';
+			echo '<td>' . $row[ "LIB_CANTIDADDISPONIBLE" ] . '</td>';
+			echo '<td>' . $row[ "AUT_NOMBRE" ] . '</td>';
+			echo '<td>' . $row[ "EDI_NOMBRE" ] . '</td>';
+			echo '<td>' . $row[ "LIB_ANIOEDICION" ] . '</td>';
+			echo '<td>' . $row[ "MAT_NOMBRE" ] . '</td>';
+			echo '<td>' . $row[ "LIB_CANTIDADPAGINAS" ] . '</td>';
+			echo '<td>' . $row[ "LIB_FORMATO" ] . '</td>';
+			echo '<td>' . $row[ "LIB_PESO" ] . '</td>';
+			echo '<td>' . $row[ "LIB_DESCIPCION" ] . '</td>';
+			echo '<td>' . $row[ "LIB_PORTADA" ] . '</td></tr>';
+		}
+	?>
+		</table>
+	
 </body>
 </html>
