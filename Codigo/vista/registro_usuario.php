2a47ab1960e48ac4d43b318eed704b7d7a0d4bea
KvnSG <kevinsuntaxi@hotmail.com>
2021-09-15 00:15:31 -0500

    autoincremento usuario

--- Codigo/vista/registro_usuario.php
+++ Codigo/vista/registro_usuario.php
@@ -10,39 +10,53 @@
 	<?php
 	$mensaje = "";
 	
-	$id = $_POST[ 'id' ]; 
-	$trabajador = $_POST[ 'codigo' ];
+	//$id = $_POST[ 'id' ]; 
+	$trabajador = $_POST[ 'trabajador' ];
 	$cedula = $_POST[ 'cedula' ];
 	$nombre = $_POST[ 'nombre' ]; 
-	$profesion = $_POST[ 'carrera' ];
+	$profesion = $_POST[ 'profesion' ];
 	$cargo = $_POST[ 'direccion' ];
-	$direccion = $_POST[ 'telefono' ]; 
-	$telefono = $_POST[ 'email' ];
+	$direccion = $_POST[ 'cargo' ]; 
+	$email = $_POST[ 'email' ];
+	$telefono = $_POST[ 'telefono' ];
 	$login = $_POST[ 'login' ];
 	$clave = $_POST[ 'clave' ];
-	$email = 1;
+	$estado = 1;
 
 	$conexion = oci_connect('BIBLIOTECA', 'bibliotecaweb', 'localhost/orcl');
 
-	$repetido = 0;
+	$usuario= oci_parse($conexion, "Select * from USUARIO");
+	$r = oci_execute($usuario);
+	$row_ini = oci_fetch_array($usuario,OCI_ASSOC);
 	
 	
-	$query_rep1 = "Select usu_nombre from usuario where usu_id = $id";
-	$stid = oci_parse($conexion, $query_rep1);
-	oci_execute($stid);
+	if($row_ini == 0){
+		$query =   "INSERT INTO usuario(usu_id, usu_trabajador, usu_cedula, usu_nombre, usu_profesion, usu_cargo, usu_direccion, usu_telefono, usu_email, usu_login, usu_clave, usu_estado) VALUES (1,'$trabajador','$cedula','$nombre','$profesion','$cargo','$direccion','$telefono','$email','$login','$clave',$estado)";
+		
+		$stid = oci_parse( $conexion, $query );
+		oci_execute( $stid );
+	}else{
 
+		$id_nuevo = oci_parse($conexion, "Select * from USUARIO");
+		$r2 = oci_execute($id_nuevo);
+		while ( $row = oci_fetch_array( $id_nuevo ) ) {
+			$aux = $row['USU_ID'];
+		}
 
+		$aux = $aux+1;
 
-	$query = "INSERT INTO usuario(usu_id, usu_trabajador, usu_cedula, usu_nombre, usu_profesion, usu_cargo, usu_direccion, usu_telefono, usu_email, usu_login, usu_clave) VALUES ($id,'$trabajador','$cedula','$nombre','$profesion','$cargo','$direccion','$telefono','$login','$clave')";
+		$query =  "INSERT INTO usuario(usu_id, usu_trabajador, usu_cedula, usu_nombre, usu_profesion, usu_cargo, usu_direccion, usu_telefono, usu_email, usu_login, usu_clave, usu_estado) VALUES ($aux,'$trabajador','$cedula','$nombre','$profesion','$cargo','$direccion','$telefono','$email','$login','$clave',$estado)";
+		$stid = oci_parse( $conexion, $query );
+		$ok = oci_execute( $stid );
 
-	$stid = oci_parse($conexion, $query);
-	$ok = oci_execute($stid);
+	}
+	
 
 	if ( $ok ) {
 		echo '<script>window.alert("Los datos se han guardado exitosamente");
 		window.location="formulario_estudiante.php";</script>';
 	} else {
-		echo "<script>window.alert('Error al ingresar los datos de Administador');window.history.go(-1);</script>";
+		echo "<script>window.alert('Error al ingresar los datos de Administador');</script>";
 	}
 
 	?>
