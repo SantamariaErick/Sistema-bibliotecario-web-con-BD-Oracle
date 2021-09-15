2b8cd0275133d45f67dc35561b4b7d411a445566
KvnSG <kevinsuntaxi@hotmail.com>
2021-09-15 01:23:18 -0500

    Auditoria

--- Codigo/vista/eliminar_materia.php
+++ Codigo/vista/eliminar_materia.php
@@ -33,6 +33,36 @@ $stid = oci_parse($conexion, $eliminar);
 $r2 = oci_execute($stid);
 
 if($r && $r2){
+	/////////////////////////////////////////////////////////////////////////////////////////////////////
+		$stid_auditoria = oci_parse($conexion, "Select * from AUDITORIA order by AUD_ID asc");
+		$r = oci_execute($stid_auditoria);
+		$row_auditoria = oci_fetch_array($stid_auditoria,OCI_ASSOC); 
+		session_start();
+		$user = $_SESSION['user'];
+
+		if($row_auditoria == 0){
+			$query_auditoria = "INSERT INTO AUDITORIA (AUD_ID, USU_ID, AUD_DESCRIPCION) VALUES (1,$user,'Actualizo usuario')";
+			$stid_aud = oci_parse( $conexion, $query_auditoria );
+			oci_execute( $stid_aud );
+		}else{
+
+			$id_nuevo_auditoria = oci_parse($conexion, "Select * from AUDITORIA order by AUD_ID asc");
+			$r2_auditoria = oci_execute($id_nuevo_auditoria);
+			while ( $row_aud = oci_fetch_array( $id_nuevo_auditoria ) ) {
+				$aux_auditoria = $row_aud['AUD_ID'];
+			}
+
+			$aux_auditoria = $aux_auditoria+1;
+
+			echo "<script>alert($aux_auditoria);</script>";
+
+			$auditoria = "INSERT INTO AUDITORIA (AUD_ID, USU_ID, AUD_DESCRIPCION) VALUES ($aux_auditoria,$user,'Modifico estado materia')";
+			$stid2 = oci_parse( $conexion, $auditoria );
+			oci_execute( $stid2 );
+		}
+		/////////////////////////////////////////////////////////////////////////////////////////////////////
+	
+	
 	echo '<script> 
 			alert("Registro '.$msj.' correctamente");
 			window.location="materia.php";
--- Codigo/vista/registro_materia.php
+++ Codigo/vista/registro_materia.php
@@ -45,6 +45,36 @@
 
 
 	if ( $ok ) {
+		/////////////////////////////////////////////////////////////////////////////////////////////////////
+		$stid_auditoria = oci_parse($conexion, "Select * from AUDITORIA order by AUD_ID asc");
+		$r = oci_execute($stid_auditoria);
+		$row_auditoria = oci_fetch_array($stid_auditoria,OCI_ASSOC); 
+		session_start();
+		$user = $_SESSION['user'];
+
+		if($row_auditoria == 0){
+			$query_auditoria = "INSERT INTO AUDITORIA (AUD_ID, USU_ID, AUD_DESCRIPCION) VALUES (1,$user,'Agrego materia')";
+			$stid_aud = oci_parse( $conexion, $query_auditoria );
+			oci_execute( $stid_aud );
+		}else{
+
+			$id_nuevo_auditoria = oci_parse($conexion, "Select * from AUDITORIA order by AUD_ID asc");
+			$r2_auditoria = oci_execute($id_nuevo_auditoria);
+			while ( $row_aud = oci_fetch_array( $id_nuevo_auditoria ) ) {
+				$aux_auditoria = $row_aud['AUD_ID'];
+			}
+
+			$aux_auditoria = $aux_auditoria+1;
+
+			echo "<script>alert($aux_auditoria);</script>";
+
+			$auditoria = "INSERT INTO AUDITORIA (AUD_ID, USU_ID, AUD_DESCRIPCION) VALUES ($aux_auditoria,$user,'Agrego materia')";
+			$stid2 = oci_parse( $conexion, $auditoria );
+			oci_execute( $stid2 );
+		}
+		/////////////////////////////////////////////////////////////////////////////////////////////////////
+		
+		
 		echo '<script>window.alert("Los datos se han guardado exitosamente");</script>';
 	} else {
 		echo "<script>window.alert('Error al ingresar los datos de Administador');window.history.go(-1);</script>";
