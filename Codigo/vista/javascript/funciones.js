b4560228d78738f4ee9394cda19964e3df076d32
ErickSantamaria <ejsantamaria1@espe.edu.ec>
2021-09-15 00:52:27 -0500

    auditoria

--- Codigo/vista/javascript/funciones.js
+++ Codigo/vista/javascript/funciones.js
@@ -7,3 +7,32 @@ function bdDateToString(cadena){
 	result = aux.join('-');
 	return result;
 }
+
+function validarCed() {
+	var cad = document.getElementById("cedula").value.trim();
+	var total = 0;
+	var longitud = cad.length;
+	var longcheck = longitud - 1;
+	if (cad !== "" && longitud === 10) {
+		for ( let i = 0; i < longcheck; i++) {
+			if (i % 2 === 0) {
+				var aux = cad.charAt(i) * 2;
+				if (aux > 9) aux -= 9;
+				total += aux;
+			} else {
+				total += parseInt(cad.charAt(i)); // parseInt o concatenará en lugar de sumar
+			}
+		}
+
+		total = total % 10 ? 10 - total % 10 : 0;
+
+		if (cad.charAt(longitud - 1) == total) {
+			window.alert("Cedula Válida");
+		} else {
+			window.alert("Cedula Inválida");
+		}
+	}
+	else{
+		window.alert("Cedula Inválida");
+	}
+}
--- Codigo/vista/registro_autor.php
+++ Codigo/vista/registro_autor.php
@@ -1,5 +1,5 @@
 <?php
-$id = $_POST[ 'id' ];
+require('../controlador/Conexion.php');
 $nombre = $_POST[ 'nombre' ];
 $descripcion = $_POST[ 'descripcion' ];
 $imagen = $_POST[ 'imagen' ];
@@ -34,9 +34,37 @@ if($row_ini == 0){
 
 }
 
-if ( $ok ) {
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+$stid_auditoria = oci_parse($conexion, "Select * from AUDITORIA");
+$r = oci_execute($stid_auditoria);
+$row_auditoria = oci_fetch_array($stid_auditoria,OCI_ASSOC); 
+session_start();
+$user = $_SESSION['user'];
+
+if($row_auditoria == 0){
+	$query_auditoria = "INSERT INTO AUDITORIA (AUD_ID, USU_ID, AUD_DESCRIPCION) VALUES (1,$user,'Registro autor')";
+	$stid_aud = oci_parse( $conexion, $query_auditoria );
+	oci_execute( $stid_aud );
+}else{
+	
+	$id_nuevo_auditoria = oci_parse($conexion, "Select * from AUDITORIA");
+	$r2_auditoria = oci_execute($id_nuevo_auditoria);
+	while ( $row_aud = oci_fetch_array( $id_nuevo_auditoria ) ) {
+		$aux_auditoria = $row_aud['AUD_ID'];
+	}
+	
+	$aux_auditoria = $aux_auditoria+1;
+	$auditoria = "INSERT INTO AUDITORIA (AUD_ID, USU_ID, AUD_DESCRIPCION) VALUES ($aux_auditoria,$user,'Registro autor')";
+	$stid2 = oci_parse( $conexion, $auditoria );
+	oci_execute( $stid2 );
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+if ( $ok &&($stid2 || $stid_aud) ) {
 	echo '<script>window.alert("Los datos se han guardado exitosamente");
-		window.location="autor.php";</script>';
+		window.location="autores.php";</script>';
 } else {
 	echo "<script>window.alert('Error al ingresar los datos');window.history.go(-1);</script>";
 }
