0469eecdf768ad6ea1dd844893082e470c322979
KvnSG <kevinsuntaxi@hotmail.com>
2021-09-14 23:16:31 -0500

    Autoincremento Editorial

--- Codigo/vista/registro_editorial.php
+++ Codigo/vista/registro_editorial.php
@@ -5,10 +5,32 @@ $nombre = $_POST[ 'nombre' ];
 $descripcion = $_POST[ 'descripcion' ];
 $estado = 1;
 
-$query = "INSERT INTO editorial(edi_id,edi_nombre, edi_descripcion,edi_estado) VALUES ($id,'$nombre','$descripcion',$estado)";
+$editorial = oci_parse($conexion, "Select * from EDITORIAL");
+$r = oci_execute($editorial);
+$row_ini = oci_fetch_array($editorial,OCI_ASSOC);
 
-$stid = oci_parse( $conexion, $query );
-$ok = oci_execute( $stid );
+
+if($row_ini == 0){
+	$query =  "INSERT INTO editorial(edi_id,edi_nombre, edi_descripcion,edi_estado) VALUES (1,'$nombre','$descripcion',$estado)";
+	
+	$stid = oci_parse( $conexion, $query );
+	oci_execute( $stid );
+}else{
+	
+	$id_nuevo = oci_parse($conexion, "Select * from EDITORIAL");
+	$r2 = oci_execute($id_nuevo);
+	while ( $row = oci_fetch_array( $id_nuevo ) ) {
+		$aux = $row['EDI_ID'];
+	}
+	
+	$aux = $aux+1;
+	
+	$query = "INSERT INTO editorial(edi_id,edi_nombre, edi_descripcion,edi_estado) VALUES ($aux,'$nombre','$descripcion',$estado)";
+	$stid = oci_parse( $conexion, $query );
+	$ok = oci_execute( $stid );
+
+}
+
 
 if ( $ok ) {
 	echo '<script>window.alert("Los datos se han guardado exitosamente");
