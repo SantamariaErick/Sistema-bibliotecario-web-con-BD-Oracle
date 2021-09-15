3b9f83230c38f2f93d0883c02922bebc3ca48b51
ErickSantamaria <ejsantamaria1@espe.edu.ec>
2021-09-14 23:15:23 -0500

    Update formulario_prestamo.php

--- Codigo/vista/formulario_prestamo.php
+++ Codigo/vista/formulario_prestamo.php
@@ -11,93 +11,11 @@
 <body>
 	<form id="form1" name="form1" method="post" action="registro_prestamo.php">
 		<h1 class="centrar titulo">Formulario prestamo</h1>
-
-<<<<<<< Updated upstream
-				<div class="row">
-					<div class="form-dividido2 formulario__grupo" id="grupo__id">
-
-					</div>
-					<div class="form-dividido2 formulario__grupo">
-						<p><label for="estudiante" class="formulario__label">Estudiante</label>
-						</p>
-						<p>
-							<select name="estudiante" id="estudiante">
-								<?php
-								require( '../controlador/Conexion.php' );
-								$stid = oci_parse( $conexion, "SELECT * FROM estudiante" );
-								if ( !$stid ) {
-									$e = oci_error( $conexion );
-									trigger_error( htmlentities( $e[ 'message' ], ENT_QUOTES ), E_USER_ERROR );
-								}
-								// Realizar la lógica de la consulta
-								$r = oci_execute( $stid );
-								if ( !$r ) {
-									$e = oci_error( $stid ); //Algun error al consultar
-									trigger_error( htmlentities( $e[ 'message' ], ENT_QUOTES ), E_USER_ERROR );
-								}
-								while ( $row = oci_fetch_array( $stid ) ) {
-									echo '<option value = "' . $row[ 0 ] . '">' . $row[ 3 ].' con CI: '.$row[2] . '</option>';
-								}
-								oci_free_statement( $stid );
-								?>
-							</select>
-						</p>
-					</div>
-					<div class="form-dividido2 formulario__grupo">
-						<p><label for="libro" class="formulario__label">Libro</label>
-						</p>
-						<p>
-							<select name="libro" id="libro">
-								<?php
-								$stid = oci_parse( $conexion, "SELECT * FROM libro" );
-								if ( !$stid ) {
-									$e = oci_error( $conexion );
-									trigger_error( htmlentities( $e[ 'message' ], ENT_QUOTES ), E_USER_ERROR );
-								}
-								// Realizar la lógica de la consulta
-								$r = oci_execute( $stid );
-								if ( !$r ) {
-									$e = oci_error( $stid ); //Algun error al consultar
-									trigger_error( htmlentities( $e[ 'message' ], ENT_QUOTES ), E_USER_ERROR );
-								}
-								while ( $row = oci_fetch_array( $stid ) ) {
-									echo '<option value = "' . $row[ 0 ] . '">' . $row[ 4 ] . '</option>';
-								}
-								oci_free_statement( $stid );
-								?>
-							</select>
-						</p>
-					</div>
-					<div class="form-dividido2 formulario__grupo" id="grupo__nombre">
-						<p><label for="nombre" class="formulario__label">Fecha prestado</label>
-						</p>
-						<input type="date" class="formulario__input" name="fpresta" id="fpresta" required>
-					</div>
-					<div class="form-dividido2 formulario__grupo">
-						<p>
-							<label for="fdevol" class="formulario__label">Fecha devolucion </label>
-						</p>
-						<input name="fdevol" type="date" required class="formulario__input" id="fdevol">
-					</div>
-					<div class="form-dividido2 formulario__grupo">
-						<p><label for="cantidad" class="formulario__label">Cantidad </label>
-						</p>
-						<input name="cantidad" type="number" required class="formulario__input" id="cantidad" max="30" min="0" step="1">
-					</div>
-					<div class="form-dividido2 formulario__grupo">
-						<p><label for="observacion" class="formulario__label">Observaciones</label>
-						</p>
-						<input name="observacion" type="text" required class="formulario__input" id="observacion">
-					</div>
-			  <div class="form-dividido2 formulario__grupo">
-				<p><label for="condicion" class="formulario__label">Estado</label>
-				  </p>
-=======
 		<div class="row">
 			<div class="form-group col-md-2">
 				<p><label for="id" class="formulario__label">ID </label>
 				</p>
-				<input type="number" class="form-control" name="id" id="id" placeholder="Ingrese el id" required>
+				<input type="number" class="form-control" name="id" id="id" placeholder="Ingrese el id">
 			</div>
 			<div class="form-group col-md-6">
 				<p><label for="estudiante" class="formulario__label">Estudiante</label>
@@ -158,7 +76,6 @@
 				<input type="date" class="form-control" name="fpresta" id="fpresta" required>
 			</div>
 			<div class="form-group col-md-4">
->>>>>>> Stashed changes
 				<p>
 					<label for="fdevol" >Fecha devolucion </label>
 				</p>
